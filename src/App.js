1748312195e424ee03cd15c418b0fed51915f2a1
Marko Kosyk <kosyk.markian@gmail.com>
2023-02-13 19:50:19 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Menu, SubMenu, Item } from "burger-menu";
+import 'burger-menu/lib/index.css';
+import burge_menu_icon from './images/menu.png';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="check">
+      <div onClick={() => setIsOpen(!isOpen)}><img src={burge_menu_icon} width="50px" height="50px"/></div>
+      <Menu className="burger-menu" isOpen={isOpen} selectedKey={'entry'} onClose={() => setIsOpen(false)}>
+        <Item itemKey={'manage'} text={'User Management'}></Item>
+        <Item itemKey={'user'} text={'User Center'}></Item>
+        <SubMenu title="Union Management">
+          <Item itemKey={'notice'} text={'Announcement'}></Item>
+          <Item itemKey={'union'} text={'Union Inquiries'}></Item>
+          <Item itemKey={'entry'} text={'Entry information'}></Item>
+        </SubMenu>
+      </Menu>
     </div>
-  );
+  )
 }
 
 export default App;
